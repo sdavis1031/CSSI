57a1bbc1abb89980eb3c6ab3eeb5a53e351debc2
Summer <desireedavis1034@gmail.com>
2017-06-28 17:16:58 -0400

    help me

---
+++ my_work.js
@@ -0,0 +1,30 @@
+console.log('this works better');
+
+var cookie= [
+  "chocolate chip",
+  "the lemon ones",
+  "thin mints",
+  "oreos",
+  "samoa",
+  "oatmeal"
+];
+
+function eat(food){
+  console.log('I just ate ' + cookies);
+}
+function eatCookies(){
+
+for (var i = 0; i<cookie.length; i = i + 1)
+eat(cookie[i]);
+}
+var eatButton= $(".devour");
+eatButton.on("click", eatCookies)
+
+
+
+
+
+
+git add - () => {
+
+}
